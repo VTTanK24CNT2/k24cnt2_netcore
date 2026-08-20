e35edd9276fa1e786ecb6b612450e927ad6dda3d
VTTanK24CNT2 <tancony1111@gmail.com>
2026-08-20 10:55:46 +0700

    VttLesson01

---
+++ VttLesson01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VttLesson01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello! Trọng Tân");
+
+            app.Run();
+        }
+    }
+}
---
+++ VttLesson01MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VttLesson01MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
