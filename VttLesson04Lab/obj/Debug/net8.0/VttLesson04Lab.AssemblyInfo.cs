2cc012660e0dec5fff222fa5f129e27edff94c75
VTTanK24CNT2 <tancony1111@gmail.com>
2026-09-16 16:04:19 +0700

    capnhatlesson

--- VttLesson04Lab/obj/Debug/net8.0/VttLesson04Lab.AssemblyInfo.cs
+++ VttLesson04Lab/obj/Debug/net8.0/VttLesson04Lab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VttLesson04Lab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2de4e050289ec44938ad61a0a40515e21d5362")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea858e1fa9b58c1960db5ad26391a36e5352d53f")]
 [assembly: System.Reflection.AssemblyProductAttribute("VttLesson04Lab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VttLesson04Lab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
